795621ea51d1ddb53d498985988d7bfd9765e6fd
Tanishq Kumar <146754449+Tanishq909@users.noreply.github.com>
2025-02-14 21:18:25 +0530

    Update db.js

--- backend/utilis/db.js
+++ backend/utilis/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
-const DbCon = async()=> {
-    try{
-       await mongoose.connect(process.env.MONGODB_URL);
-       console.log("MongoDB is connected");
-    }
-    catch{
-       console.log(error);
+const DbCon = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MongoDB is connected");
+    } catch (error) {
+        console.log(error);
     }
 }
 
